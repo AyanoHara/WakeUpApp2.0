d49fc485f75e08c3b5e511d26231c219f0ce33c3
AYANO HARA <ayanohara@AYANOnoMacBook-Air.local>
2020-05-17 14:06:20 +0900

    first commit

---
+++ WakeUpApp(2)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WakeUpApp(2)
+//
+//  Created by AYANO HARA on 2020/05/17.
+//  Copyright © 2020 AYANO HARA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

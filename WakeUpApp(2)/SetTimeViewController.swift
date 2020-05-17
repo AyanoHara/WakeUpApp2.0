93319a3990f489d81857772247194f13712361c5
AYANO HARA <ayanohara@AYANOnoMacBook-Air.local>
2020-05-17 17:07:24 +0900

    直しました

--- WakeUpApp(2)/SetTimeViewController.swift
+++ WakeUpApp(2)/SetTimeViewController.swift
@@ -11,7 +11,6 @@ import UIKit
 class SetTimeViewController: UIViewController {
     
     @IBOutlet weak var sleepTimePicker: UIDatePicker!
-    
     @IBOutlet weak var setTimeButton: UIButton!
     
     override func viewDidLoad() {
